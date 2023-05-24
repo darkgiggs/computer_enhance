577e46e459dbfabedc6a3f82fe6a17896529af56
giggs <darkgiggsxx@gmail.com>
2023-05-24 10:16:09 +0200

    Add missing assignment features
    Use std::format for easier json writing.
    Implement square clusters, cluster count depends on the pair count.
    Write the computation results to a binary file.

--- perfaware/part2/generator.cpp
+++ perfaware/part2/generator.cpp
@@ -1,69 +1,145 @@
-#include "haversine_formula.cpp"
-#include "types.h"
 #include <random>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <iomanip>
-#include <sstream>
+#include <format>
+#include "types.h"
+#include "haversine_formula.h"
 
-std::mt19937_64 rng;
-std::uniform_real_distribution<f64> longitude(-180.0, 180.0);
-std::uniform_real_distribution<f64> latitude(-90.0, 90.0);
+std::mt19937_64 prng;
+constexpr f64 CLUSTER_RANGE = 30.0;
 constexpr f64 EARTH_RADIUS = 6372.8;
 
-void GeneratePair(f64 points[4])
+struct Point2
 {
-	points[0] = longitude(rng);
-	points[1] = latitude(rng);
-	points[2] = longitude(rng);
-	points[3] = latitude(rng);
-}
+	f64 x;
+	f64 y;
+};
 
-void AddPairToJsonBuffer(std::string& buffer, f64 points[4])
+Point2 clusterCenters[64];
+
+f64 RandomDouble(f64 min, f64 max)
 {
-	std::stringstream stream;
-	
-	stream << std::fixed << std::setprecision(16) 
-		<< "\t{\"x0\":"
-		<< points[0]
-		<< ", \"y0\":"
-		<< points[1]
-		<< ", \"x1\":"
-		<< points[2]
-		<< ", \"y1\":"
-		<< points[3]
-		<< "},\n";
-	
-	buffer += stream.str();
+	std::uniform_real_distribution<f64> getrandom(min, max);
+	return getrandom(prng);
 }
-
-
 
 int main(int argCount, char** args)
 {
+	bool properUsage = true;
+	bool generateClusters = false;
+	u64 seed = 0;
+	u64 pairCount = 0;
+	u32 clusterCount = 1;
+
+	if (argCount == 4)
+	{
+		if (strcmp(args[1], "cluster") == 0)
+		{
+			generateClusters = true;
+		}
+		else if (strcmp(args[1], "uniform") != 0)
+		{
+			properUsage = false;
+		}
+		seed = atoll(args[2]);
+		pairCount = atoll(args[3]);
+	}
+	else
+	{
+		properUsage = false;
+	}
+
+	if (!properUsage || !pairCount)
+	{
+		std::cout 
+			<< "Usage: generator.exe [uniform/cluster] [random seed] [number of coordinate pairs to generate]\n" 
+			<< std::endl;
+		return 0;
+	}
+
+	prng.seed(seed);
+
+	f64* haversineDistances = static_cast<f64*>(malloc((pairCount + 1) * sizeof(f64)));
+
+	if (generateClusters)
+	{
+		u64 counter = pairCount >> 1;
+		while (counter)
+		{
+			clusterCount++;
+			counter >>= 1;
+		}
+
+		for (u32 i = 0; i < clusterCount; i++)
+		{
+			clusterCenters[i].x = RandomDouble(-180.0 + CLUSTER_RANGE, 180.0 - CLUSTER_RANGE);
+			clusterCenters[i].y = RandomDouble(-90.0 + CLUSTER_RANGE, 90.0 - CLUSTER_RANGE);
+		}
+	}
+
 	f64 expectedSum = 0.0;
-	u64 pairCount = 10;
-	std::string outputBuffer;
-
-	outputBuffer += "{\"pairs\":[\n";
-
-	f64 points[4];
+	std::ofstream json("pairs.json", std::ofstream::out);
+	if (!json.good())
+	{
+		std::cout << "Error opening json file" << std::endl;
+		return 0;
+	}
+	
+	json << "{\"pairs\":[\n";
 
 	for (u64 i = 0; i < pairCount; i++)
 	{
-		GeneratePair(points);
-		AddPairToJsonBuffer(outputBuffer, points);
-		expectedSum += ReferenceHaversine(points[0], points[1], points[2], points[3], EARTH_RADIUS);
+		f64 x0, y0, x1, y1;
+
+		if (generateClusters)
+		{
+			u64 clusterIndex = i * clusterCount / pairCount;
+			Point2* clusterCenter = &clusterCenters[clusterIndex];
+
+			x0 = RandomDouble(clusterCenter->x - CLUSTER_RANGE, clusterCenter->x + CLUSTER_RANGE);
+			x1 = RandomDouble(clusterCenter->x - CLUSTER_RANGE, clusterCenter->x + CLUSTER_RANGE);
+			y0 = RandomDouble(clusterCenter->y - CLUSTER_RANGE, clusterCenter->y + CLUSTER_RANGE);
+			y1 = RandomDouble(clusterCenter->y - CLUSTER_RANGE, clusterCenter->y + CLUSTER_RANGE);
+		}
+		else
+		{
+			x0 = RandomDouble(-180.0, 180.0);
+			x1 = RandomDouble(-180.0, 180.0);
+			y0 = RandomDouble(-90.0, 90.0);
+			y1 = RandomDouble(-90.0, 90.0);
+		}
+		json << std::vformat("\t{{\"x0\":{:.16f}, \"y0\":{:.16f}, \"x1\":{:.16f}, \"y1\":{:.16f}}}{}\n",
+			std::make_format_args(x0, y0, x1, y1, i == (pairCount - 1) ? "" : ","));
+		
+		f64 haversineDistance = ReferenceHaversine(x0, y0, x1, y1, EARTH_RADIUS);
+		expectedSum += haversineDistance;
+		haversineDistances[i] = haversineDistance;
 	}
-	outputBuffer[outputBuffer.size() - 2] = '\n';
-	outputBuffer[outputBuffer.size() - 1] = ']';
-	outputBuffer.push_back('}');
 	
-	std::ofstream json("pairs.json", std::ofstream::out);
-	json << outputBuffer;
+	json << "]}";
 	json.close();
 
-	std::cout << std::fixed << std::setprecision(16) << expectedSum / pairCount;
+	f64 average = expectedSum / pairCount;
+	haversineDistances[pairCount] = average;
 
+	std::ofstream binaryDistances("answer.f64", std::ofstream::out | std::ofstream::binary);
+	binaryDistances.write(reinterpret_cast<char*>(haversineDistances), sizeof(f64) * (pairCount + 1));
+	binaryDistances.close();
+
+#if _DEBUG
+	std::ifstream test("answer.f64", std::ifstream::in | std::ifstream::binary);
+	f64 value;
+	test.seekg(sizeof(f64) * pairCount);
+	test.read(reinterpret_cast<char*>(&value), sizeof(value));
+	if (value != average)
+	{
+		std::cout << "Computed sum/stored sum mismatch" << std::endl;
+	}
+	test.close();
+#endif
+
+	std::cout << std::vformat(
+		"Method: {}\nRandom seed: {}\nPair count: {}\nExpected sum: {:.16f}",
+		std::make_format_args(generateClusters ? "cluster" : "uniform", seed, pairCount, average))
+		<< std::endl;
 }
